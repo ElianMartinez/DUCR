7f250ae75f482609485b3d76dd49dfd659f4054a
Elian Martinez <elianmartinez157@gmail.com>
2019-10-30 08:36:35 -0400

    kdajflshvsabj;va

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,17 +1,23 @@
 import {Component} from '@angular/core';
 
 
+
 @Component({
 	selector: 'login',
 	templateUrl: './login.component.html',
 	styleUrls: ['./login.css']
 })
 
+
+
 export class loginComponent{
 	public titulo = 'login';
 	
 
-	
+	save(){
+
+	}
+
 	private pre($event){
 		$event.preventDefault();
 		console.log("se detuvo");
