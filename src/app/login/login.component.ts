947a873cd97d597c76fba8a8eae9b7bcfc5c9121
Elian Martinez <elianmartinez157@gmail.com>
2019-10-30 08:37:33 -0400

    error

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -4,7 +4,7 @@ import {Component} from '@angular/core';
 
 @Component({
 	selector: 'login',
-	templateUrl: './login.component.html',
+	templateUrl: './login.component.html'
 	styleUrls: ['./login.css']
 })
 
