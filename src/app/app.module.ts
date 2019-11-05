0590cf04035b6bfda567078d905524e14611ca99
Elian Martinez <elianmartinez157@gmail.com>
2019-11-05 09:34:24 -0400

    Agregando el Perfil

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { routing, appRoutingProviders }   from './app.routing';
 import { AppComponent } from './app.component';
 import { homeComponent } from './home/home.component';
 import { loginComponent } from './login/login.component';
+import { PerfilComponent } from './perfil/perfil.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     homeComponent,
-    loginComponent
+    loginComponent,
+    PerfilComponent
   ],
   imports: [
     BrowserModule,
